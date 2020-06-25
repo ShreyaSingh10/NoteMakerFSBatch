2751afbf6a256e74f44a7d1a130384d6af3a9772
ShreyaSingh10 <shreyasssingh10@gmail.com>
2020-06-25 12:12:02 +0530

    AddNote functionality

--- src/Components/AddNotes.js
+++ src/Components/AddNotes.js
@@ -1,19 +1,31 @@
 import React from 'react';
 
+//Child
+
 export default class AddNotes extends React.Component {
 
-	state= {
-		name: ''
+	state={
+		name: ""
 	}
 
 	handleChange = (e) => {
-		console.log("Handling changes");
-		const name= e.target.value; //important
-		console.log("NAME", name);
-		this.setState({name: name}, ()=> console.log("STATE", this.state));
+		const name= e.target.value; //important /fetching the data from the input element
+		this.setState({name: name}); //setting in state (one way)
+	}
+
+	handleSubmit = (e) => {
+		console.log("SUBMITTING");
+		e.preventDefault(); // use this whenever u are working with forms to stop
+		//page from reloading
+		//child parent se communicate kare - doesnt mean child has to return a value to parent
+		const note = {
+			name: this.state.name
+		}
+		this.props.addNote(note);
 	}
 
 	render(){
+		//console.log("PROPS", this.props);
 		return(
 			<div>
 				<form onSubmit={this.handleSubmit}>
--- src/Components/Container.js
+++ src/Components/Container.js
@@ -1,23 +1,44 @@
 import React from 'react';
-import AddNotes from './AddNotes';
+import AddNotes from './AddNotes'; //we have imported child component AddNotes into our 
+//parent component -> Container
+
+//Parent
 
 export default class Container extends React.Component {
 	state ={
-		name: "first app"
+		notes:[] //suppose notes has 4 notes already
 	}
 
-	addNote = () => {
-
+	//addNote functionality
+	addNote = (note) => {
+		//console.log("name", name);
+		this.setState(prevstate => { //second way to set the state
+			let newArr = [...prevstate.notes]; //make a shallow copy of already 
+			//existing  notes array in your state
+			newArr.push(note); //pushing new object inside the duplicate array
+			return {
+				notes: newArr //setting the state
+			}
+		})
 	}
 
 	render(){
+		console.log("State", this.state);
 		return(
 			<div>
 				{this.state.name}
-				<AddNotes />
+				<AddNotes 
+					addNote={this.addNote} 
+				/>
 			</div>
 		)
 	}
 }
 
 //export default Container;
+
+
+//How can child communicate with parent?
+//1. there shud be a function in the parent
+//2. that function should be sent to the child as props
+//3. Child should use/call the function and pass the value to the function
